7a29984a83d65213899ac331df5b79876ce938e2
darkziny22 <darkziny22@nate.com>
2013-12-10 18:35:45 +0900

    fuxxing closer

--- camera.py
+++ camera.py
@@ -2,10 +2,19 @@ import os
 import sys
 import time
 
+set_device_video = "uv4l --driver raspicam --auto-video_nr --framerate 25"
+
+
 def camera_execute():
 
 	now = time.localtime()
 	filename = str(now.tm_year)+str(now.tm_mon)+str(now.tm_mday)+'-'+str(now.tm_hour)+str(now.tm_min)+str(now.tm_sec)
+
+        if not os.path.exists("/dev/video0"):
+                print("video device is not found")
+                os.system(set_device_video)
+        else:
+                print("video device is exist")
 
 	os.system('raspistill -o ./static/' + filename + '.jpg')
 
@@ -13,7 +22,7 @@ def camera_execute():
 	return 'http://' + get_ip.get_ip_address('eth0') + ':5000/static/' + filename + '.jpg'
 
 def view_stream():
-	set_device_video = "uv4l --driver raspicam --auto-video_nr --framerate 25"
+
 	video_stream = "cvlc v4l2:///dev/video0 --v4l2-width 640 --v4l2-height 480 --v4l2-chroma h264 --sout '#rtp{sdp=rtsp://:8554/}'"
 
 	if not os.path.exists("/dev/video0"):

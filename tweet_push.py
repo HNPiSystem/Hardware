a3b59cd49db727ae233de4bea0780bbd26c1f634
darkziny22 <darkziny22@nate.com>
2013-12-10 18:34:51 +0900

    fuxxing closer

--- tweet_push.py
+++ tweet_push.py
@@ -1,12 +1,16 @@
-import tweepy
+#-*- coding:utf-8 -*-
+
+import tweepy, time
 from tweepy import OAuthHandler
 
 C_KEY = "Jh5WdFZkgLKg1wv4m3FrQ"
 C_SECRET = "Rb1a5aK33jyRJTvFRDkbnnL8rw2onUhffPNvueVKmBE"
 
 oauth = tweepy.OAuthHandler(C_KEY, C_SECRET)
-
 oauth_url = oauth.get_authorization_url()
+
+now = time.localtime()
+make_time = str(now.tm_year) + "년" + str(now.tm_mon) + "월" + str(now.tm_mday) + "일 " + str(now.tm_hour) + ":" + str(now.tm_min) + ":" + str(now.tm_sec)
 #print('Open the URL : ' + oauth_url)
 
 #input_pin = input('PIN : ')
@@ -22,4 +26,5 @@ A_SECRET = 'vew3UAxcP6uJg19PQ4dug1smu3Xazt7aTKy0SVx1dXm1X'
 
 oauth.set_access_token(A_KEY, A_SECRET)
 api = tweepy.API(auth_handler=oauth, api_root='/1.1', secure=True)
-api.update_status('PIR Sensor detect motion! Check your home!')
+
+api.update_status(make_time + '\nPIR Sensor detect motion! Check your home!')
